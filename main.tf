73b580affab2f79d63fe85778fefb4f4f154fb32
Shikha Maheshwari <shikhamaheshwari@Shikhas-MacBook-Pro.local>
2024-01-31 18:11:00 +0530

    fix: temporary changes for tf16

--- main.tf
+++ main.tf
@@ -11,3 +11,15 @@ resource "ibm_is_ssh_key" "ssh_key" {
   public_key     = resource.tls_private_key.tls_key.public_key_openssh
   tags           = var.tags
 }
+
+# Create a VPC
+resource "ibm_is_vpc" "vpc" {
+  name                        = "${var.name}-vpc"
+  resource_group              = var.resource_group_id
+  classic_access              = var.classic_access
+  default_network_acl_name    = "${var.name}-edge-acl"
+  default_security_group_name = "${var.name}-default-sg"
+  default_routing_table_name  = "${var.name}-default-table"
+  address_prefix_management   = "manual"
+  tags                        = var.tags
+}
