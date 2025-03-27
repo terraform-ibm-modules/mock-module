afaaed54f8674458544bf38935241ecb6a268ee5
Daniel Butler <dannyeb@gmail.com>
2025-03-27 13:10:32 +0000

    add nop dep

--- tests/manual_addon_test.go
+++ tests/manual_addon_test.go
@@ -4,14 +4,16 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testaddons"
 )
 
 func setupAddonOptions(t *testing.T, prefix string, dir string) *testaddons.TestAddonOptions {
 	options := testaddons.TestAddonsOptionsDefault(&testaddons.TestAddonOptions{
-		Testing:       t,
-		Prefix:        prefix,
-		ResourceGroup: resourceGroup,
+		Testing:              t,
+		Prefix:               prefix,
+		ResourceGroup:        resourceGroup,
+		DeployTimeoutMinutes: 10,
 	})
 
 	return options
@@ -20,7 +22,14 @@ func setupAddonOptions(t *testing.T, prefix string, dir string) *testaddons.Test
 func TestRunBasicAddon(t *testing.T) {
 	t.Parallel()
 
-	options := setupAddonOptions(t, "mock-basic", basicExampleTerraformDir)
+	options := setupAddonOptions(t, "mock-standard", basicExampleTerraformDir)
+	options.AddonConfig.OfferingFlavor = "standard"
+	options.AddonConfig.OfferingName = "deploy-arch-ibm-mock-module"
+	options.AddonConfig.OfferingInstallKind = *cloudinfo.NewInstallKindTerraform()
+	options.AddonConfig.Inputs = map[string]interface{}{
+		"region": "us-east",
+		"prefix": options.Prefix,
+	}
 
 	err := options.RunAddonTest()
 	assert.Nil(t, err, "This should not have errored")
