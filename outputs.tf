de920e3510abb40f2d52802132586544d319630e
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-04-11 10:46:05 +0530

    push some temp code to test renovate

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "fingerprint" {
   description = "SSH key Fingerprint info"
   value       = tls_private_key.tls_key.public_key_fingerprint_sha256
 }
+
+output "audit_webhook_listener_image_version" {
+  description = "audit_webhook_listener_image_version"
+  value       = var.audit_webhook_listener_image_version
+}
--- variables.tf
+++ variables.tf
@@ -14,3 +14,14 @@ variable "tags" {
   type        = list(string)
   default     = []
 }
+
+variable "audit_webhook_listener_image_version" {
+  type        = string
+  description = "The tag or digest for the audit webhook listener image to deploy. If changing the value, ensure it is compatible with `audit_webhook_listener_image`."
+  nullable    = false
+  default     = "122e7a6c887ef24fea3095518857a708bd84b1e4@sha256:04c08891cd0201e11ecc6fe4f7b016ed137989010ced987cab57be5c7af3915a" # datasource: icr.io/ibm/ibmcloud-kube-audit-to-ibm-cloud-logs
+  validation {
+    condition     = can(regex("^[a-f0-9]{40}@sha256:[a-f0-9]{64}$", var.audit_webhook_listener_image_version))
+    error_message = "The value of the audit webhook listener image version must match the tag and sha256 image digest format"
+  }
+}
