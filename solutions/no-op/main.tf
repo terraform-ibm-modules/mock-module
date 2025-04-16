0f32bfa9fe94447bd8b6c0f32922979f11f9c482
Daniel Butler <dannyeb@gmail.com>
2025-04-16 15:53:41 +0100

    add extra inputs for test

--- solutions/no-op/main.tf
+++ solutions/no-op/main.tf
@@ -1,7 +1,27 @@
-
 #############################################################################
 # Call root level module to create SSH key
 #############################################################################
+
+locals {
+  prefix           = var.prefix
+  no-op-nul-number = var.no-op-nul-number
+  notset-number    = var.notset-number
+  no-op-nul-bool   = var.no-op-nul-bool
+  notset-bool      = var.notset-bool
+  no-op-nul-list   = var.no-op-nul-list
+  notset-list      = var.notset-list
+  no-op-nul-map    = var.no-op-nul-map
+  notset-map       = var.notset-map
+  no-op-nul-set    = var.no-op-nul-set
+  notset-set       = var.notset-set
+  no-op-nul-object = var.no-op-nul-object
+  notset-object    = var.notset-object
+  no-op-nul-tuple  = var.no-op-nul-tuple
+  notset-tuple     = var.notset-tuple
+  region           = var.region
+  no-op-nul-string = var.no-op-nul-string
+  notset-string    = var.notset-string
+}
 
 module "mock_module" {
   source = "../.."
--- solutions/no-op/variables.tf
+++ solutions/no-op/variables.tf
@@ -1,5 +1,106 @@
 variable "prefix" {
   type        = string
   description = "Prefix to append to all resources created by this example"
-  default     = "mock-no-op"
+}
+
+variable "no-op-nul-number" {
+  type        = number
+  description = "No-op variable to test setting a number to null"
+  default     = null
+}
+
+variable "notset-number" {
+  type        = number
+  description = "No-op variable to test not setting a number"
+}
+
+variable "no-op-nul-bool" {
+  type        = bool
+  description = "No-op variable to test setting a bool to null"
+  default     = null
+}
+
+variable "notset-bool" {
+  type        = bool
+  description = "No-op variable to test not setting a bool"
+}
+
+variable "no-op-nul-list" {
+  type        = list(string)
+  description = "No-op variable to test setting a list to null"
+  default     = null
+}
+
+variable "notset-list" {
+  type        = list(string)
+  description = "No-op variable to test not setting a list"
+}
+
+variable "no-op-nul-map" {
+  type        = map(string)
+  description = "No-op variable to test setting a map to null"
+  default     = null
+}
+
+variable "notset-map" {
+  type        = map(string)
+  description = "No-op variable to test not setting a map"
+}
+
+variable "no-op-nul-set" {
+  type        = set(string)
+  description = "No-op variable to test setting a set to null"
+  default     = null
+}
+
+variable "notset-set" {
+  type        = set(string)
+  description = "No-op variable to test not setting a set"
+}
+
+variable "no-op-nul-object" {
+  type        = object({
+    name = string
+    age  = number
+  })
+  description = "No-op variable to test setting an object to null"
+  default     = null
+}
+
+variable "notset-object" {
+  type        = object({
+    name = string
+    age  = number
+  })
+  description = "No-op variable to test not setting an object"
+}
+
+variable "no-op-nul-tuple" {
+  type        = tuple([string, number])
+  description = "No-op variable to test setting a tuple to null"
+  default     = null
+}
+
+variable "notset-tuple" {
+  type        = tuple([string, number])
+  description = "No-op variable to test not setting a tuple"
+}
+
+variable "region" {
+  type        = string
+  description = "Region to provision all resources created by this example"
+  default     = "us-south"
+}
+
+variable "no-op-nul-string" {
+  type        = string
+  description = "No-op variable to test setting a string to null"
+  default     = null
+  
+}
+
+variable "notset-string" {
+  type        = string
+  description = "No-op variable to test not setting a string"
+  
 }
