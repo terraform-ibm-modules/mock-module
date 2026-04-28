dd2912099f955150b945c822db94d869377995c1
Zikra Iqbal <35029744+zikra-iqbal@users.noreply.github.com>
2026-04-28 11:08:45 +0530

    fix: update code to test renovate rule for Mastercard/restapi (#1017)
    * chore: testing Mastercard/restapi rule
    
    * fix: pre-commit hook

--- examples/complete/version.tf
+++ examples/complete/version.tf
@@ -5,5 +5,10 @@ terraform {
       source  = "IBM-Cloud/ibm"
       version = ">= 1.58.1"
     }
+    # tflint-ignore: terraform_unused_required_providers
+    restapi = {
+      source  = "Mastercard/restapi"
+      version = "2.0.1"
+    }
   }
 }
