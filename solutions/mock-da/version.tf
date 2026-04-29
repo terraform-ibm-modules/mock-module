c876be3474f9e9f6dea4fbb1a7d127b5d00ce0e2
Zikra Iqbal <35029744+zikra-iqbal@users.noreply.github.com>
2026-04-29 09:32:08 +0530

    chore: revert temporary changes (#1022)

--- solutions/mock-da/version.tf
+++ solutions/mock-da/version.tf
@@ -3,12 +3,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "2.0.1"
-    }
-    # tflint-ignore: terraform_unused_required_providers
-    restapi = {
-      source  = "Mastercard/restapi"
-      version = "2.0.1"
+      version = "2.0.2"
     }
   }
 }
